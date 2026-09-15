ef9413c781de27a45b450202accfe396dd6f6afc
J-MCD3115 <oozeki@jeansmcd.co.jp>
2026-09-15 14:41:55 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -140,24 +140,22 @@ $(function () {
 
   //モーダルウィンドウ
   const openButtons = document.querySelectorAll(".modal-open");
-const closeButtons = document.querySelectorAll(".modal-close");
-//→開く
-openButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modalId = button.dataset.modal;
-    const modal = document.getElementById(modalId);
+  const closeButtons = document.querySelectorAll(".modal-close");
+  //→開く
+  openButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modalId = button.dataset.modal;
+      const modal = document.getElementById(modalId);
 
-    modal.classList.add("is-open");
+      modal.classList.add("is-open");
+    });
   });
-});
-//→閉じる
-closeButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = button.closest(".modal");
+  //→閉じる
+  closeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.closest(".modal");
 
-    modal.classList.remove("is-open");
+      modal.classList.remove("is-open");
+    });
   });
-});
-
-  
 });
